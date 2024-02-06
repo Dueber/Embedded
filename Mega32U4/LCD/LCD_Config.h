047ce52cc20f59e057353e1f522815fe54e52c2a
Christopher Dueber <dueberct@gmail.com>
2024-02-05 22:18:38 -0800

    Update LCD_Config.h

--- Mega32U4/LCD/LCD_Config.h
+++ Mega32U4/LCD/LCD_Config.h
@@ -6,7 +6,8 @@
 #define LCD_DISPLAY_NORMAL				0b10100100	
 #define LCD_DISPLAY_ALL					0b10100101
 
-#define LCD_PAGE_ADDR_SET				0b10110000			
+#define LCD_PAGE_ADDR_SET				0b10110000				// D4:0 select pages 8-0
+#define LCD_LINE_ADDR_SET				0b01000000
 #define LCD_CLMN_ADDR_SET_UPPER			0b00010000
 #define LCD_CLMN_ADDR_SET_LOWER			0b00000000
 
@@ -32,9 +33,13 @@ void LCD_CLEAR_SCREEN (void);
 
 void LCD_NORMAL (void);
 
-void LCD_SET_COLUMN_UPPER (uint8_t addr);
+void LCD_SET_PAGE_ADDR (void);
 
-void LCD_SET_COLUMN_LOWER (uint8_t addr);
+void LCD_SET_COLUMN_UPPER (void);
+
+void LCD_SET_COLUMN_LOWER (void);
+
+void LCD_SET_LINE_ADDR (void);
 
 void LCD_SET_START_WRITE (uint8_t addr);
 
